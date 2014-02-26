203bc402b3bb2efb5398a75bb729170158687bc6
Jonathan Hudson <jh+github@daria.co.uk>
2014-02-26 20:14:51 +0000

    trivial makeall script change

--- makeall.sh
+++ makeall.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+PRECLEAN=
+POSTCLEAN=
+
+for ARG
+do
+  case $ARG in
+    pre*) PRECLEAN=clean ;;
+    post*) POSTCLEAN=clean ;;
+  esac
+done
 
 CVERS=$(pkg-config --modversion champlain-0.12)
-
 case $CVERS in
 0.12.[0-3]) TGT=mwpu ;;
 *) TGT=mwp ;;
@@ -11,11 +20,9 @@ esac
 for P in mspsim  pidedit switchedit
 do
   cd $P
-  make clean install-local
-  make clean
+  make $PRECLEAN install-local $POSTCLEAN
   cd ..
 done
 
 cd mwp
-make clean $TGT install-local
-make clean
+make $PRECLEAN $TGT install-local $POSTCLEAN
