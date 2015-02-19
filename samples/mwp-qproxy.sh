b3ebf88ccc6bf1aa6b444942771c28885f9ad52e
Jonathan Hudson <jh+github@daria.co.uk>
2015-02-19 19:59:22 +0000

    add old libsoup workaround

--- samples/mwp-qproxy.sh
+++ samples/mwp-qproxy.sh
@@ -2,6 +2,8 @@
 
 ########## Only needed for old libsoup, e.g. Ubuntu 14.04 or earlier ########
 # add your port (from sources.json) and URI
+# but make sure you keep the ampersand at the end &&&&&
+#                vvv
 qproxy #port #URI &
 qpid=$!
 mwp
