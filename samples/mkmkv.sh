037d2833496677e7460cf3c1b836fe4fc018f6e3
Jonathan Hudson <jh+github@daria.co.uk>
2015-11-29 13:30:46 +0000

    improve mkmkv.sh for other environments

--- samples/mkmkv.sh
+++ samples/mkmkv.sh
@@ -11,12 +11,20 @@ then
  sleep 2
 fi
 
-wmctrl -F -r 'mwp' -e 0,32,32,1280,720
+# Change the screen size to suite your needs / hardware here
+# note also prorata values in ffmpeg command line.
+XRES=1280
+YRES=720
+YYRES=$((YRES + 36))
+wmctrl -F -r 'mwp' -e 0,32,32,$XRES,$YRES
 
 F=pulse
 S=$(pactl list | grep monitor | grep Name: | cut -d ' ' -f 2)
 
 read -p "Hit ENTER to start > "
 
-ffmpeg -y -f $F -i $S -f x11grab -acodec pcm_s16le -r 30 -s 1280x756 \
+# note older versions of Ubuntu / Debian may need avconv rather than ffmpeg
+# FFMPEG=avconv
+FFMPEG=ffmpeg
+$FFMPEG -y -f $F -i $S -f x11grab -acodec pcm_s16le -r 30 -s ${XRES}x${YYRES} \
  -i ${DISPLAY}+32,32 -vcodec libx264 -crf 0 -preset ultrafast -threads 0 $OUT
