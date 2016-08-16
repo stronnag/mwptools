f21e022fd991804921bda4284d116c47f0222145
Jonathan Hudson <jh+github@daria.co.uk>
2016-08-16 22:48:02 +0100

    add 'universal' flash script

--- samples/flash.sh
+++ samples/flash.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# if your distro doen't provide it 
+# if your distro doen't provide it
 # git://git.code.sf.net/p/stm32flash/code
 
-DEV=/dev/ttyUSB0
+DEV=
 SPEED=115200
-HEX=obj/cleanflight_NAZE.hex
+HEX=
 RESCUE=
+BIN=
 
 for P
 do
@@ -14,14 +15,32 @@ do
     [123456789]*) SPEED=$P ;;
     /dev/*) DEV=$P ;;
     *.hex) HEX=$P ;;
+    *.bin) BIN=$P ;;    
     rescue) RESCUE=1 ;;
   esac
 done
+
+if [ -z "$DEV" ]
+then
+ [ -n "$HEX" ] && DEV=/dev/ttyUSB0 || DEV=/dev/ttyACM0
+fi
+
+echo $DEV
 
 stty -F $DEV raw speed $SPEED -crtscts cs8 -parenb -cstopb -ixon
 if [ -z "$RESCUE" ]
 then
   echo -n 'R' >$DEV
-  sleep 0.2
+  sleep 0.5
 fi
-stm32flash -w $HEX -v -g 0x0 -b $SPEED $DEV
+
+if [ -n "$HEX" ]
+then
+ stm32flash -w $HEX -v -g 0x0 -b $SPEED $DEV
+fi
+
+
+if [ -n "$BIN" ]
+then
+ dfu-util -d 0483:df11  --alt 0 -s 0x08000000:leave -D $BIN
+fi
