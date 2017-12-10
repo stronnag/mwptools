7aced427e226fe5c5ea63c779083af9519329fec
Jonathan Hudson <jh+github@daria.co.uk>
2017-12-10 11:07:50 +0000

    update version no generation

--- mwp/mkvers.sh
+++ mwp/mkvers.sh
@@ -2,13 +2,12 @@
 
 dstr=$(date +%F)
 txt=$(git log -1 --format="%h" 2>/dev/null || echo "local")
-if [ "$txt" == "local" ]
-then
-  # From the master source tree
-  y=$(($(date +%y)-17))
-  mwpid=$y.$(date +%j).$(($(date +%s)%86400/100))
-   echo "char * mwpid=\"$mwpid\";" > mwpid.h
-fi
 txt="$txt / $dstr"
+if [ -f .master ] ; then
+ y=$(($(date +%y)-17))
+ mwpid=$y.$(date +%j).$(($(date +%s)%86400/100))
+ echo "char * mwpid=\"$mwpid\";" > mwpid.h
+fi
+
 echo "char *mwpvers=\"$txt\";" > mwpvers.h
-echo "#include \"mwpid.h\"" >> mwpvers.h
+cat "mwpid.h" >> mwpvers.h
