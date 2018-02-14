fe5b49e2d85e92ffe2c22011719d725d34f9b97a
Jonathan Hudson <jh+github@daria.co.uk>
2018-02-14 22:17:42 +0000

    python sample is now as functional as the ruby example

--- samples/mwp-dbus.py
+++ samples/mwp-dbus.py
@@ -18,10 +18,23 @@ device_list = mwp.GetDevices()
 for dev in device_list:
     print("    [" + dev + "]")
 
+pts=0
 try:
     filename = sys.argv[1]
+    pts = mwp.LoadMission(filename)
+    print("Loaded " + filename + " with " + str(pts) + " mission points")
 except IndexError:
-    sys.exit(2)
+    print("no mission given")
 
-pts = mwp.LoadMission(filename)
-print("Loaded " + filename + " with " + str(pts) + " mission points")
+constat = mwp.ConnectionStatus()
+if constat[1]:
+    print("Connected to "+constat[0])
+else:
+    print("Not connected")
+    constat = mwp.ConnectDevice(device_list[0])
+
+if constat[-1]:
+    print("now connected to "+device_list[0])
+    if pts > 0:
+        nwpts = mwp.UploadMission(True)
+        print("Uploaded " + str(nwpts) + " waypoints to the FC")
