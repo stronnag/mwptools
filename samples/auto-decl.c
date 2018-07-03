3d9487dc88287cd6533e55eb58c3df8087ae7e3d
Jonathan Hudson <jh+github@daria.co.uk>
2018-07-03 18:59:07 +0100

    add stdint.h to samples/auto-decl.c so it cross -compiles for win32 with ancient cross-compiler

--- samples/auto-decl.c
+++ samples/auto-decl.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdint.h>
 /*
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
