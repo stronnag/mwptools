7f6f49a56f4cca30b47ed751860a26c2d9ce5e03
Jonathan Hudson <jh+github@daria.co.uk>
2018-07-20 18:17:12 +0100

    fixup tzget.sh

--- samples/tzget.sh
+++ samples/tzget.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-cd ~/Projects/ZoneDetect-master
+# Using ZoneDetect https://github.com/BertoldVdb/ZoneDetect
+# based on data from https://github.com/evansiroky/timezone-boundary-builder
+# the latter has links to other implementations
+#
+# ZoneDetect databses:
+#  timezone16.bin has a longitude resolution of 0.0055 degrees (~0.5km).
+#  timezone21.bin has a longitude resolution of 0.00017 degrees (~20m).
+#
+# Example installation
+# sudo cp library/libzonedetect.so /usr/local/lib
+# sudo cp -a  database /usr/local/share/zonedectect
+# sudo cp zdemo /usr/local/bin/zone-detect
+# sudo ldconfig
+#
 
+DATABASE=/usr/local/share/zonedectect/timezone21.bin
 ZONE=
 while read K V
 do
@@ -9,5 +23,5 @@ do
     TimezoneIdPrefix:) ZONE=$V ;;
     TimezoneId:) ZONE=${ZONE}${V} ;;
   esac
-done < <(LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./library ./zdemo database/timezone21.bin $1 $2)
+done < <(./zone-detect $DATABASE $1 $2)
 echo $ZONE
