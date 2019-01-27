4f6bfe559ba76c17cf4edf6fd63cfd97c4aefa29
Jonathan Hudson <jh+github@daria.co.uk>
2019-01-27 11:35:55 +0000

    states for 2.x, update inav_modes.rb for ruby 2.6

--- bbox-replay/inav_modes.rb
+++ bbox-replay/inav_modes.rb
@@ -64,43 +64,49 @@ IO.popen(cmd,'r') do |p|
 		:header_converters =>
 		->(f) {f.strip.downcase.gsub(' ','_').gsub(/\W+/,'').to_sym},
 		:return_headers => true)
-  hdrs = csv.shift
+
+  nhdr=false
+  itn=0
+  nstate = -1
+  istate = 'IDLE'
   st = nil
   xts=nil
   ts=nil
-  nstate = -1
-  istate = 'IDLE'
-  itn=0
 
   csv.each do |c|
-    ts = c[:time_us].to_f / 1000000
-    itn = c[:loopiteration]
-    st = ts if st.nil?
-    xts  = ts - st
-    fs = c[:failsafephase_flags].strip
-    if fs != istate
-      istate = fs
-      rcd=''
-      # Old logs don't have rcData[]
-      if c.has_key? :rcdata0
-	rcd = " (#{[c[:rcdata0].strip,c[:rcdata1].strip,c[:rcdata2].strip,c[:rcdata3].strip, ].join(',')})"
+    if  nhdr == false
+      hdrs = c
+      nhdr = true
+    else
+      ts = c[:time_us].to_f / 1000000
+      itn = c[:loopiteration]
+      st = ts if st.nil?
+      xts  = ts - st
+      fs = c[:failsafephase_flags].strip
+      if fs != istate
+	istate = fs
+	rcd=''
+	# Old logs don't have rcData[]
+	if c.has_key? :rcdata0
+	  rcd = " (#{[c[:rcdata0].strip,c[:rcdata1].strip,c[:rcdata2].strip,c[:rcdata3].strip, ].join(',')})"
+	end
+	rcx=[c[:rxsignalreceived].strip,c[:rxflightchannelsvalid].strip].join(',')
+	if rcd.empty?
+	  rcd = " (#{rcx})"
+	else
+	  rcd << ';' << "(#{rcx})"
+	end
+	puts ["%9d" % itn, "%6.1f" % ts, "(%6.1f)" % xts, "F/S=#{istate}#{rcd}"].join("\t")
       end
-      rcx=[c[:rxsignalreceived].strip,c[:rxflightchannelsvalid].strip].join(',')
-      if rcd.empty?
-	rcd = " (#{rcx})"
-      else
-	rcd << ';' << "(#{rcx})"
+      if c[:navstate].to_i != nstate
+	if nstate == -1
+	  puts %w/Iteration Time(s) Elapsed(s)  State/.join("\t")
+	end
+	nstate = c[:navstate].to_i
+	as = INAV_STATES[inavers][c[:navstate].to_i].to_s
+	astate = (as) ? "#{as} (#{nstate})" : "State=%d" % nstate
+	puts ["%9d" % itn, "%6.1f" % ts, "(%6.1f)" % xts, astate].join("\t")
       end
-      puts ["%9d" % itn, "%6.1f" % ts, "(%6.1f)" % xts, "F/S=#{istate}#{rcd}"].join("\t")
-    end
-    if c[:navstate].to_i != nstate
-      if nstate == -1
-	puts %w/Iteration Time(s) Elapsed(s)  State/.join("\t")
-      end
-      nstate = c[:navstate].to_i
-      as = INAV_STATES[inavers][c[:navstate].to_i].to_s
-      astate = (as) ? "#{as} (#{nstate})" : "State=%d" % nstate
-      puts ["%9d" % itn, "%6.1f" % ts, "(%6.1f)" % xts, astate].join("\t")
     end
   end
   puts ["%9d" % itn, "%6.1f" % ts, "(%6.1f)" % xts, "end of log"].join("\t")
