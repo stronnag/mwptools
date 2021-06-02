c209646a00d14f782ac4ae2fff573fde9541ce83
Jonathan Hudson <jh+github@daria.co.uk>
2021-06-02 17:24:21 +0100

    support jump missions in mwp-plot-elevations

--- src/mwp-plot-elevations/mission.go
+++ src/mwp-plot-elevations/mission.go
@@ -42,10 +42,6 @@ type Mission struct {
 	MissionItems []MissionItem `xml:"missionitem"̀`
 }
 
-var (
-	needrth int
-)
-
 func NewMission(fname string) (*Mission, error) {
 	var mission Mission
 	r, err := os.Open(fname)
@@ -66,6 +62,22 @@ func NewMission(fname string) (*Mission, error) {
 	return &mission, err
 }
 
+func find_best_alt(mpts []Point, wpno int) {
+	ba := -99999
+	for _, m := range mpts {
+		if m.Wpno == int8(wpno) {
+			if m.Xz > ba {
+				ba = m.Xz
+			}
+		}
+	}
+	for j, m := range mpts {
+		if m.Wpno == int8(wpno) {
+			mpts[j].Xz = ba
+		}
+	}
+}
+
 func (m *Mission) Save(mpts []Point) {
 	w, err := os.Create(Conf.Output)
 	if err == nil {
@@ -74,11 +86,12 @@ func (m *Mission) Save(mpts []Point) {
 		m.Version.Value = "0.0-rc0"
 		m.MwpMeta.Stamp = time.Now().UTC().Format(time.RFC3339)
 		m.MwpMeta.Generator = "mwp-plot-elevations"
+
 		for _, mi := range m.MissionItems {
-			if mi.Action == "LAND" {
+			if mi.Action == "LAND" && landno == -1 {
 				landno = int8(mi.No)
-				break
 			}
+			find_best_alt(mpts, mi.No)
 		}
 
 		for _, p := range mpts {
@@ -157,86 +170,122 @@ func (m *Mission) check_for_home() error {
 
 func (m *Mission) Get_points() []Point {
 	mpts := []Point{}
-	mnpts := len(m.MissionItems)
+	valid := false
+	lx := 0.0
+	ly := 0.0
+	cx := 0.0
+	cy := 0.0
+	nsize := len(m.MissionItems)
+	ret := false
+
 	if Homep.Set == WP_HOME {
-		if m.MissionItems[mnpts-1].Action == "RTH" {
-			needrth = mnpts - 2
-		}
+		Homep.Wpname = "HOME"
 		mpts = append(mpts, Homep)
+		mpts[0].Flag = 0
+		mpts[0].Set = WP_HOME
+		valid = true
+		cx = Homep.X
+		cy = Homep.Y
+		lx = cx
+		ly = cy
 	}
-	for _, mi := range m.MissionItems {
-		if mi.Is_GeoPoint() {
-			mpts = append(mpts, Point{Y: mi.Lat, X: mi.Lon})
+	dist := 0.0
+	jumpC := make([]int16, nsize)
+
+	for j, mi := range m.MissionItems {
+		if mi.Action == "JUMP" {
+			jumpC[j] = mi.P2
 		}
 	}
-	if needrth != 0 {
-		mpts = append(mpts, Homep)
+	n := 0
+	for {
+		if n >= nsize {
+			break
+		}
+		var typ = m.MissionItems[n].Action
+		if valid {
+			if typ == "SET_POI" || typ == "SET_HEAD" {
+				n += 1
+				continue
+			}
+			if typ == "JUMP" {
+				if jumpC[n] == -1 {
+					n += 1
+				} else {
+					if jumpC[n] == 0 {
+						jumpC[n] = m.MissionItems[n].P2
+						n += 1
+					} else {
+						jumpC[n] -= 1
+						n = int(m.MissionItems[n].P1) - 1
+					}
+				}
+				continue
+			}
+
+			if typ == "RTH" {
+				ret = true
+				break
+			}
+			mi := m.MissionItems[n]
+			cy = mi.Lat
+			cx = mi.Lon
+			_, dm := geo.Csedist(ly, lx, cy, cx)
+			dist += dm * 1852.0
+			mpts = append(mpts, Point{Y: cy, X: cx, Wpno: int8(mi.No), D: dist,
+				Wpname: fmt.Sprintf("WP%d", mi.No), Flag: int8(mi.P3), Set: WP_INIT,
+				Mz: int(mi.Alt)})
+			n += 1
+		} else {
+			cy = m.MissionItems[n].Lat
+			cx = m.MissionItems[n].Lon
+			valid = true
+			n += 1
+		}
+		lx = cx
+		ly = cy
+	}
+	if ret {
+		_, dm := geo.Csedist(ly, lx, Homep.Y, Homep.X)
+		dist += dm * 1852.0
+		mpts = append(mpts, Point{Y: Homep.Y, X: Homep.X, Wpno: -1, Wpname: "RTH", D: dist,
+			Flag: 0, Set: WP_RTH})
 	}
 	return mpts
 }
 
 func (m *Mission) Update_details(mpts []Point, elev []int) {
 	n := 0
-	d := 0.0
-	llat := Homep.Y
-	llon := Homep.X
 
-	if Homep.Set == WP_HOME {
-		mpts[0].Wpno = 0
-		mpts[0].Wpname = "Home"
+	if mpts[0].Flag == WP_HOME {
 		mpts[0].Gz = elev[0]
 		mpts[0].Mz = 0
 		mpts[0].Az = elev[0]
 		mpts[0].Xz = elev[0]
-		mpts[0].Flag = 0
-		mpts[0].D = 0
-		mpts[0].Set = WP_HOME
 		n = 1
 	}
 
-	for _, mi := range m.MissionItems {
-		if mi.Is_GeoPoint() {
-			av := int(mi.Alt)
-			mpts[n].Wpno = int8(mi.No)
-			mpts[n].Wpname = fmt.Sprintf("WP%d", mi.No)
+	for ; n < len(mpts); n += 1 {
+		if mpts[n].Wpno == -1 {
+			mpts[n].Gz = elev[0]
+			mpts[n].Mz = Conf.Rthalt
+			mpts[n].Az = elev[0] + Conf.Rthalt
+			mpts[n].Xz = mpts[n].Az
+		} else {
 			mpts[n].Gz = elev[n]
-			mpts[n].Flag = int8(mi.P3)
-			if mi.P3 == 0 {
-				mpts[n].Mz = av
-				mpts[n].Az = elev[0] + av
+			alt := mpts[n].Mz
+			if mpts[n].Flag == 0 {
+				mpts[n].Az = elev[0] + alt
 			} else {
-				mpts[n].Mz = av - elev[0]
-				mpts[n].Az = av
+				mpts[n].Mz -= elev[0]
+				mpts[n].Az = alt
 			}
 			if Conf.Noalts {
 				mpts[n].Xz = elev[n] + Conf.Margin
 			} else {
 				mpts[n].Xz = mpts[n].Az
 			}
-			mpts[n].Set = WP_INIT
-
-			if llat != 0.0 && llon != 0.0 {
-				_, dm := geo.Csedist(llat, llon, mi.Lat, mi.Lon)
-				d += dm * 1852.0
-			}
-			mpts[n].D = d
-			llat = mi.Lat
-			llon = mi.Lon
-			n += 1
 		}
-	}
-	if needrth > 0 {
-		mpts[n].Wpno = -1
-		mpts[n].Wpname = "RTH"
-		mpts[n].Gz = elev[0]
-		mpts[n].Mz = Conf.Rthalt
-		mpts[n].Az = elev[0] + Conf.Rthalt
-		mpts[n].Xz = mpts[n].Az
-		mpts[n].Flag = 0
-		mpts[n].Set = WP_RTH
-		_, dm := geo.Csedist(Homep.Y, Homep.X, m.MissionItems[needrth].Lat, m.MissionItems[needrth].Lon)
-		d += dm * 1852.0
-		mpts[n].D = d
 	}
 }
 
