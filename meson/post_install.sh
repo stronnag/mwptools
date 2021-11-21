7e47d7537e09e0fa7d052e8e2496933f1d706960
Jonathan Hudson <jh+github@daria.co.uk>
2021-11-21 16:02:14 +0000

    update build for dh_make

--- meson/post_install.sh
+++ meson/post_install.sh
@@ -1,14 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-ln -f ${DESTDIR}/${MESON_INSTALL_PREFIX}/bin/fc-get  ${DESTDIR}/${MESON_INSTALL_PREFIX}/bin/fc-set
+INSTDIR=${MESON_INSTALL_PREFIX}
+if [ -n "$DESTDIR" ] ; then
+    INSTDIR=${DESTDIR}/${MESON_INSTALL_PREFIX}
+fi
 
-# Go programs, without whinging
-for FN in mwp-plot-elevations bproxy ublox-geo ublox-cli flashdl ; do
-  if [ -e  $MESON_BUILD_ROOT/$FN ] ; then
-    rm -f $MESON_INSTALL_PREFIX/bin/$FN
-    install -C $MESON_BUILD_ROOT/$FN $MESON_INSTALL_PREFIX/bin
-  fi
+ln -f ${INSTDIR}/bin/fc-get  ${INSTDIR}/bin/fc-set
+
+# Additional programs, without whinging
+for FN in bproxy ublox-geo ublox-cli flashdl ; do
+    if [ -e  $MESON_BUILD_ROOT/$FN ] ; then
+	rm -f $INSTDIR/bin/$FN
+	install -C $MESON_BUILD_ROOT/$FN $INSTDIR/bin
+    fi
 done
+
+if [ -e ${MESON_SOURCE_ROOT}/src/mwp-plot-elevations/mwp-plot-elevations ] ; then
+    install ${MESON_SOURCE_ROOT}/src/mwp-plot-elevations/mwp-plot-elevations  $INSTDIR/bin
+fi
 
 if [ -z $DESTDIR ]; then
   echo Compiling gsettings schemas...
