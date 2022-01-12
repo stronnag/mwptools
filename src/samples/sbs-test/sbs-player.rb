6485a30bfedef8d90757da431fb441221c9a1c56
Jonathan Hudson <jh+github@daria.co.uk>
2022-01-12 20:02:14 +0000

    add SBS log replay tool

---
+++ src/samples/sbs-test/sbs-player.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+require 'optparse'
+require 'time'
+
+def handle_client(s, fnam)
+  Thread.new do
+    lts = nil
+    peer = "#{s.peeraddr[3]}:#{s.peeraddr[1]}"
+    File.open(fnam) do |fh|
+      fh.each do |l|
+        l.chomp!
+        a = l.split(',')
+        ts  = Time.parse([a[8],a[9]].join(' '))
+        sleep ts - lts if lts
+        s.puts l rescue break
+        lts = ts
+      end
+    end
+    STDERR.puts "--- Close session #{peer}"
+    s.close rescue nil
+  end
+end
+
+
+host='::'
+port=30003
+ARGV.options do |opt|
+  opt.banner = "Usage: netcap.rb [options] file"
+  opt.on('-p','--port PORT',Integer, port) {|o| port=o}
+  opt.parse!
+end
+fnam = ARGV.shift
+server = TCPServer.new(host,port)
+server.setsockopt(Socket::SOL_SOCKET, Socket::SO_REUSEADDR,1)
+while (session = server.accept)
+  peer = "#{session.peeraddr[3]}:#{session.peeraddr[1]}"
+  STDERR.puts "++ New session #{peer}"
+  handle_client(session, fnam)
+end
