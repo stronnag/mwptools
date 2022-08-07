029ace079d22d1cdc128120c884f4892a0ee241d
Jonathan Hudson <jh+github@daria.co.uk>
2022-08-07 17:50:22 +0100

    Revert "use meson gnome module"
    This reverts commit 8f9b31e337f22d4d3fca69bb6a075d20a178bf32.

--- meson/post_install.sh
+++ meson/post_install.sh
@@ -22,3 +22,17 @@ fi
 if [ -e ${MESON_SOURCE_ROOT}/src/samples/flashgo/flashgo ] ; then
     install ${MESON_SOURCE_ROOT}/src/samples/flashgo/flashgo  $INSTDIR/bin
 fi
+
+if [ -z $DESTDIR ]; then
+  echo >&2 Compiling gsettings schemas ...
+  glib-compile-schemas $MESON_INSTALL_PREFIX/share/glib-2.0/schemas
+
+  echo >&2 Updating desktop icon cache ...
+  gtk-update-icon-cache -qtf $MESON_INSTALL_PREFIX/share/icons/hicolor
+
+  echo >&2 Updating mime database ...
+  update-mime-database $MESON_INSTALL_PREFIX/share/mime
+
+  echo >&2  Updating desktop database ...
+  update-desktop-database  $MESON_INSTALL_PREFIX/share/applications
+fi
