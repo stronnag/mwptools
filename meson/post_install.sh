ea2c69572c0fef06e4145cc773e926b7e69cd385
Jonathan Hudson <jh+github@daria.co.uk>
2023-03-16 10:38:50 +0000

    improve build scripts

--- meson/post_install.sh
+++ meson/post_install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Ugly, but necesary to force getting correct build info
-rm -f ${MESON_SOURCE_ROOT}/build/src/mwp/_mwpvers.h
+rm -f ${MESON_BUILD_ROOT}/src/mwp/_mwpvers.h
 
 INSTDIR=${MESON_INSTALL_PREFIX}
 if [ -n "$DESTDIR" ] ; then
