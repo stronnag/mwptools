d736d7468cf42255e39db190d81fc9bc7c0ac71b
Jonathan Hudson <jh+github@daria.co.uk>
2024-11-28 20:51:13 +0000

    add local/win32 localtime_r

--- src/clib/rserial.c
+++ src/clib/rserial.c
@@ -327,4 +327,11 @@ char *get_error_text(int dummy, char *pBuf, size_t bufSize) {
   return (pBuf);
 }
 
+struct tm *localtime_r(const time_t *restrict timep, struct tm *r) {
+  struct tm *v = localtime(timep);
+  r = calloc(sizeof(*v));
+  *r = *v;
+  return r;
+}
+
 #endif
