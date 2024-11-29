021d3338ab69713b74d4c443bfa1b1b97203653f
Jonathan Hudson <jh+github@daria.co.uk>
2024-11-29 10:21:58 +0000

    localtime_r update

--- src/clib/rserial.c
+++ src/clib/rserial.c
@@ -331,7 +331,7 @@ char *get_error_text(int dummy, char *pBuf, size_t bufSize) {
 
 struct tm *localtime_r(const time_t *restrict timep, struct tm *r) {
   struct tm *v = localtime(timep);
-  r = calloc(sizeof(*v));
+  r = malloc(sizeof(*v));
   *r = *v;
   return r;
 }
