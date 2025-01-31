8605344994657f36290941a6b8b6275d0f55fe24
Jonathan Hudson <jh+github@daria.co.uk>
2025-01-31 11:00:15 +0000

    use `dc` for computation in resize-icons

--- src/samples/adsb-extra/resize-icons.sh
+++ src/samples/adsb-extra/resize-icons.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-NUM=${1:-1}
-DEN=${2:-1}
-
-for F in *svg ; do
+FC=${1:-1}
+if [[ $# -gt 1 ]] ; then
+  IC=${2:-1}
+  FC=$(dc -e "2 k $FC $IC / p")
+fi
+for F in *.svg ; do
   R=$(identify -format "%w %h\n" $F)
   W=$(echo $R | cut -d ' ' -f 1)
   H=$(echo $R | cut -d ' ' -f 2)
-  W1=$(($W*$NUM/$DEN))
-  H1=$(($H*$NUM/$DEN))
+  W1=$(dc -e "2 k $W  $FC * p")
+  H1=$(dc -e "2 k $H  $FC * p")
   echo $F $W $H $W1 $H1
   mv $F _$F
   rsvg-convert -w $W1 -h $H1 -a -f svg -o $F _$F
